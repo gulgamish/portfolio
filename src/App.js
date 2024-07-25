7883fda5077d3c968b57f6f189432bc917883f84
Ayman <mac@jumper.local>
2024-07-25 20:57:28 +0100

    update CV, add school project fillit, remove facebook link

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ import { AnimatePresence } from "framer-motion";
 const App = () => {
 
 
-    return (
-        <ChakraProvider theme={theme}>
-            <Layout>
-            <AnimatePresence exitBeforeEnter initial={true}>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="*" element={<Page404 />} />
-                </Routes>
-            </AnimatePresence>
-            </Layout>
-        </ChakraProvider>
-    )
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default App;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,56 +1,79 @@
-import { Container, Divider, Heading, SimpleGrid } from "@chakra-ui/react"
-import { GridItem, ProjectGridItem } from "../components/grid-item"
-import Section from "../components/section"
-import Wrapper from "../components/layouts/Wrapper"
+import { Container, Divider, Heading, SimpleGrid } from "@chakra-ui/react";
+import { GridItem, ProjectGridItem } from "../components/grid-item";
+import Section from "../components/section";
+import Wrapper from "../components/layouts/Wrapper";
 
-import corewarThumbNail from "../../src/assets/corewar.png"
-import matchaThumbNail from "../../src/assets/matcha.png"
-import otalentThumbnail from "../../src/assets/otalent.png"
-import minishellThumbnail from "../../src/assets/minishell.gif"
+import corewarThumbNail from "../../src/assets/corewar.png";
+import matchaThumbNail from "../../src/assets/matcha.png";
+import otalentThumbnail from "../../src/assets/otalent.png";
+import minishellThumbnail from "../../src/assets/minishell.gif";
+import FillerThumbnail from "../../src/assets/filler.gif";
 
 const Projects = () => {
-
-
-    return (
-        <Wrapper>
-            <Container mt={10} maxW="container.lg">
-                <Heading as="h3" fontSize={27} variant="headline-text">
-                    Projects
-                </Heading>
-                <SimpleGrid gap={6} columns={[1, 1, 2]}>
-                    <Section delay={.4}>
-                        <GridItem title="O-Talent" href="" thumbnail={otalentThumbnail}>
-                            The goal of the project is the Digitalization of the OCP Human Resources
-                            Management and it consisted of multiple Modules
-                        </GridItem>
-                    </Section>
-                    <Section delay={.4}>
-                        <GridItem title="Matcha" href="" thumbnail={matchaThumbNail}>
-                            Matcha is a dating website (tinder like)
-                        </GridItem>
-                    </Section>
-                </SimpleGrid>
-                <Section delay={.3}>
-                    <Divider my={6} />
-                    <Heading as="h3" variant="headline-text">
-                        School Projects
-                    </Heading>
-                </Section>
-                <SimpleGrid gap={6} columns={[1, 1, 2]}>
-                    <Section delay={.4}>
-                        <GridItem title="corewar" type="algorithm" href='https://google.com' thumbnail={corewarThumbNail}>
-                            Re-Programing of Core War, it's a 1984 programming game created by D. G. Jones and A. K. Dewdney in which two or more battle programs (called "warriors") compete for control of a virtual computer. These battle programs are written in an abstract assembly language called Redcode
-                        </GridItem>
-                    </Section>
-                    <Section delay={.4}>
-                        <GridItem title="Minishell" type="system" href="" thumbnail={minishellThumbnail}>
-                            This project is all about recreating my very own (mini)shell.
-                        </GridItem>
-                    </Section>
-                </SimpleGrid>
-            </Container>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Container mt={10} maxW="container.lg">
+        <Heading as="h3" fontSize={27} variant="headline-text">
+          Projects
+        </Heading>
+        <SimpleGrid gap={6} columns={[1, 1, 2]}>
+          <Section delay={0.4}>
+            <GridItem title="O-Talent" href="" thumbnail={otalentThumbnail}>
+              The goal of the project is the Digitalization of the OCP Human
+              Resources Management and it consisted of multiple Modules
+            </GridItem>
+          </Section>
+          <Section delay={0.4}>
+            <GridItem title="Matcha" href="" thumbnail={matchaThumbNail}>
+              Matcha is a dating website (tinder like)
+            </GridItem>
+          </Section>
+        </SimpleGrid>
+        <Section delay={0.3}>
+          <Divider my={6} />
+          <Heading as="h3" variant="headline-text">
+            School Projects
+          </Heading>
+        </Section>
+        <SimpleGrid gap={6} columns={[1, 1, 2]}>
+          <Section delay={0.4}>
+            <GridItem
+              title="corewar"
+              type="algorithm"
+              href="https://google.com"
+              thumbnail={corewarThumbNail}
+            >
+              Re-Programing of Core War, it's a 1984 programming game created by
+              D. G. Jones and A. K. Dewdney in which two or more battle programs
+              (called "warriors") compete for control of a virtual computer.
+              These battle programs are written in an abstract assembly language
+              called Redcode
+            </GridItem>
+          </Section>
+          <Section delay={0.4}>
+            <GridItem
+              title="Minishell"
+              type="system"
+              href=""
+              thumbnail={minishellThumbnail}
+            >
+              This project is all about recreating my very own (mini)shell.
+            </GridItem>
+          </Section>
+          <Section delay={0.5}>
+            <GridItem
+              title="Filler"
+              type="Algorithm"
+              thumbnail={FillerThumbnail}
+            >
+              This project is about crafting an intelligent program that fill
+              the maximum number of cases before the other program
+            </GridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default Projects;
